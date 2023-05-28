fd5d4c2312b1d6437701c993788fbc049d6e4c6b
Morgan Mason <morganmason0606@gmail.com>
2023-05-28 16:31:40 -0700

    fix sym link ?

--- ext2-create.c
+++ ext2-create.c
@@ -423,7 +423,7 @@ void write_inode_table(int fd) {
 	hello_inode.i_gid = 1000 ;
 	hello_inode.i_links_count = 1;
 	hello_inode.i_blocks = 2; /* These are oddly 512 blocks */
-	hello_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
+	hello_inode.i_block[0] = HELLO_WORLD_INO;
 	write_inode(fd, HELLO_INO, &hello_inode);
 
 }
